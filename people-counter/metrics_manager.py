3276b4d07995a3b7d12db3ef4848e9ff92b47ece
Tomás Migone <tomas@balena.io>
2020-02-18 12:59:37 -0300

    people-counter: fix MQTT messaging
    Change-type: patch
    Signed-off-by: Tomás Migone <tomas@balena.io>

--- people-counter/metrics_manager.py
+++ people-counter/metrics_manager.py
@@ -1,5 +1,5 @@
 import time
-import paho.mqtt.client as mqtt
+import paho.mqtt.publish as publish
 import json
 
 '''
@@ -13,18 +13,6 @@ class MetricsManager:
         self.ACTIVE_IDs = []
         self.START = int(time.time())
         self.ALL_USERS = {}
-        
-        # MQTT sends data to balenaSense
-        self.mqtt = mqtt.Client()
-        self.mqtt.connect("mqtt", 1883, 60)
-        self.mqtt.on_connect = self.on_connect
-        self.mqtt.on_message = self.on_message
-
-    def on_connect(client, userdata, flags, rc):
-        print("Connected with result code "+str(rc))
-
-    def on_message(client, userdata, msg):
-        print(msg.topic+" "+str(msg.payload))
 
     def newLoop(self):
         self.ACTIVE_IDs = []
@@ -41,8 +29,9 @@ class MetricsManager:
             self.ALL_USERS[objectId] = elapsed
             
             # If new person detected, send to balenaSense
-            data=json.dumps({"sepalLength": "6.4","sepalWidth":  "3.2","petalLength": "4.5","petalWidth":  "1.5"})
-            self.mqtt.publish('sensors', payload=data, qos=0, retain=False)
+            print("New person detected!")
+            publish.single("sensors", payload=json.dumps({ "person" : True }), qos=2, hostname="mqtt", port=1883)
+
         else:
             self.ALL_USERS[objectId] += elapsed
 
